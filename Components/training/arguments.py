ae197f0328536dc0c6b5215f0683ca2c1d950ae8
Joonas Kujala <joonas.kujala@hotmail.com>
2025-03-19 12:31:50 +0200

    added switch for plot saving interval

--- Components/training/arguments.py
+++ Components/training/arguments.py
@@ -20,16 +20,16 @@ def parse_args():
     parser.add_argument('--k_folds', type=int, choices=[5, 10], default=2)
     parser.add_argument('--n_splits', type=int, default=1)
     parser.add_argument('--lr', type=float, choices=[1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 5, 10],
-                        default=1e-5)  # Can increase
+                        default=1e-1)  # Can increase
     parser.add_argument('--lr_drop', type=aslist, choices=[[160, 260]], default=[160, 260])
-    parser.add_argument('--wd', type=float, choices=[5e-4, 1e-4, 1e-3, 1e-2], default=5e-2)
-    parser.add_argument('--optimizer', type=str, choices=['adam', 'sgd'], default='adam')
+    parser.add_argument('--wd', type=float, choices=[5e-4, 1e-4, 1e-3, 1e-2], default=5e-4)
+    parser.add_argument('--optimizer', type=str, choices=['adam', 'sgd'], default='sgd')
     parser.add_argument('--set_nesterov', default=True)
-    parser.add_argument('--learning_rate_decay', type=float, choices=[0.1, 0.2, ], default=0.2)
-    parser.add_argument('--n_epochs', type=int, choices=[2, 5, 10, 25, 50], default=10)  # max 50 for tests
+    parser.add_argument('--learning_rate_decay', type=float, choices=[0.1, 0.2, ], default=0.1)
+    parser.add_argument('--n_epochs', type=int, choices=[2, 5, 10, 25, 50], default=25)  # max 50 for tests
     parser.add_argument('--n_threads', type=int, default=8)
     parser.add_argument('--transform', type=bool, default=False)
     parser.add_argument('--train_fold', type=int, choices=[-1], default=-1)
-
+    parser.add_argument('--save_interval', type=int, default=5)
     args = parser.parse_args()
     return args
