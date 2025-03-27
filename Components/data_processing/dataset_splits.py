4334be9980ae7b1c2fdb1d470b9d0709273edd72
Joonas Kujala <joonas.kujala@hotmail.com>
2025-03-27 16:30:19 +0200

    Renamed split->dataset. image.transpose(0,2) -> image.transpose((2, 0, 1))

--- Components/data_processing/dataset_splits.py
+++ Components/data_processing/dataset_splits.py
@@ -5,27 +5,24 @@ import solt.core as slc
 import solt.transforms as slt
 import cv2
 import os
-from PIL import Image
-from numpy.ma.core import concatenate
 from sklearn.model_selection import StratifiedGroupKFold, StratifiedShuffleSplit, GroupShuffleSplit
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 from Components.data_processing import transformations
 
 
 class ImageDataset(Dataset):
-    def __init__(self, split, transforms=None):
-        self.dataframe = split
+    def __init__(self, dataset, transforms=None):
+        self.dataset = dataset
         self.transform = transforms
 
     def __len__(self):
-        return len(self.dataframe)
+        return len(self.dataset)
 
     def __getitem__(self, idx):
         if isinstance(idx, torch.Tensor):
             idx = idx.item()
-        entry = self.dataframe.iloc[idx]
+        entry = self.dataset.iloc[idx]
         image_path = entry.Image_path
         label = entry.Class
         folderid = entry.FolderID
@@ -37,9 +34,9 @@ class ImageDataset(Dataset):
             image = self.transform(image)
             return ""
         else:
-            image = np.array(image).astype(np.float32)
+            image = np.array(image)
+            image = image.transpose((2, 0, 1)) # numpy image: H x W x C, torch image: C x H x W
             image = torch.from_numpy(image)
-            image = image.transpose(0, 2)
 
             return {'image': image, 'label': label, 'folderid': folderid, 'image_path': image_path}
 
@@ -74,7 +71,7 @@ def initiate_sgkf_splits(args, metadata):
 
     sgkf = StratifiedGroupKFold(n_splits=args.k_folds)
 
-    y = concatenate_column_values(dframe=metadata, cols=['FolderID'])
+    y = concatenate_column_values(dframe=metadata, cols=['Class'])
     sgkf_split = sgkf.split(metadata, y=y, groups=metadata.FolderID.astype(str))
 
     cv_split = [x for x in sgkf_split]
@@ -86,7 +83,7 @@ def split_train_holdout(args, metadata, metadata_folder, dataset='histo'):
 
     if args.subsample:
         metadata = metadata.groupby('Class').apply(
-            lambda x: x.sample(frac=0.005)
+            lambda x: x.sample(frac=0.04)
         )
 
     gss = GroupShuffleSplit(n_splits=args.n_splits, test_size=0.2, train_size=0.8, random_state=args.seed)
@@ -100,34 +97,33 @@ def split_train_holdout(args, metadata, metadata_folder, dataset='histo'):
     train_metadata = metadata.iloc[training_index]
     holdout_metadata = metadata.iloc[holdout_index]
 
-    os.makedirs(metadata_folder, exist_ok=True)
     train_metadata.to_csv(os.path.join(metadata_folder, f"{dataset}_train_metadata.csv"), index=None)
     holdout_metadata.to_csv(os.path.join(metadata_folder,f"{dataset}_holdout_metadata.csv"), index=None)
 
     return train_metadata
 
 
-def init_folds(args, cv_split):
-    cv_split_train_val = {}
-
-    for fold_id, split in enumerate(cv_split):
-        if fold_id != args.train_fold and args.train_fold > -1:
-            continue
-        cv_split_train_val[fold_id] = split
-
-    return cv_split_train_val
+# def init_folds(args, cv_split):
+#     cv_split_train_val = {}
+#
+#     for fold_id, split in enumerate(cv_split):
+#         if fold_id != args.train_fold and args.train_fold > -1:
+#             continue
+#         cv_split_train_val[fold_id] = split
+#
+#     return cv_split_train_val
 
 
 def init_loaders(args, train_split, val_split):
     transformations = init_transformations(args)
 
     if args.transform:
-        train_dataset = ImageDataset(split=train_split, transforms=transformations['train'])
-        val_dataset = ImageDataset(split=val_split)
+        train_dataset = ImageDataset(dataset=train_split, transforms=transformations['train'])
+        val_dataset = ImageDataset(dataset=val_split)
 
     else:
-        train_dataset = ImageDataset(split=train_split)
-        val_dataset = ImageDataset(split=val_split)
+        train_dataset = ImageDataset(dataset=train_split)
+        val_dataset = ImageDataset(dataset=val_split)
 
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.n_threads, drop_last=True,
                               worker_init_fn=lambda wid: np.random.seed(np.uint32(torch.initial_seed() + wid)), pin_memory=True)
@@ -137,6 +133,7 @@ def init_loaders(args, train_split, val_split):
 
 
 def init_transformations(args):
+    #args here if you want to alter these.
     rotation_range = (-5, 5)
     train_trsf = slc.Stream([
         slt.Flip(p=0.25, axis=-1),
